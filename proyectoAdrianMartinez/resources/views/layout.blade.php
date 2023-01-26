57780a6b387848256d552804bd58846424ab9d4e
adronmarsh <adrianmargil8@gmail.com>
2023-01-26 19:52:53 +0100

    UPDATED FILES - 26/01/2023

--- proyectoAdrianMartinez/resources/views/layout.blade.php
+++ proyectoAdrianMartinez/resources/views/layout.blade.php
@@ -3,27 +3,48 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="author" content="Adrián Martínez">
+    <meta name="description" content="Portfolio de Adrián Martínez">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CSS-->
     <link rel="stylesheet" href="/css/style.css">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!--Iconos Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&family=Space+Mono&display=swap"
+        rel="stylesheet">
+    <!--Título-->
     <title> @yield('title') - Ágora eSports</title>
+    <!--Icono de la página-->
+    <link rel="icon" type="image/x-icon" href="/media/logo.jpeg">
 </head>
 
 <body>
+
     <div class="container">
-    <header>
-        @include('partials.header')
-    </header>
+        <header>
+            @include('partials.header')
+        </header>
 
-        @include('partials.sidebar')
+        {{-- @include('partials.sidebar') --}}
 
-        <main>
+        {{-- <main>
             @yield('content')
-        </main>
+        </main> --}}
 
-        <footer>
+        {{-- <footer>
             @include('partials.foot')
-        </footer>
+        </footer> --}}
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- proyectoAdrianMartinez/resources/views/partials/header.blade.php
+++ proyectoAdrianMartinez/resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="header">
+{{-- <nav class="header">
     <img src="/media/logo.jpeg" alt="Logo de Ágora eSports">
     <div><a href="/">Ágora eSports</a></div>
     <ul>
@@ -6,4 +6,34 @@
         <li>Login</li>
         <li>Logout</li>
     </ul>
-</nav>
+</nav> --}}
+
+<nav class="navbar navbar-expand-md bg-body-tertiary bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img id="logoAgora" src="/media/logo.jpeg" alt="Logo de Ágora eSports"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#desplegable"
+                aria-controls="desplegable" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+      <div class="collapse navbar-collapse" id="desplegable">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Eventos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Miembros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contactos</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
